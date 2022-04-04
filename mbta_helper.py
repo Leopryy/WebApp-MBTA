bc3f0a00c34ed47e349116714e938e03124c3baf
Leopryy <98495479+Leopryy@users.noreply.github.com>
2022-04-04 16:14:58 -0400

    finished web app

--- mbta_helper.py
+++ mbta_helper.py
@@ -17,8 +17,13 @@ def get_json(url):
 
     Both get_lat_long() and get_nearest_station() might need to use this function.
     """
-    pass
+    import urllib.request
+    import json
 
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    return response_data
 
 def get_lat_long(place_name):
     """
@@ -27,8 +32,12 @@ def get_lat_long(place_name):
     See https://developer.mapquest.com/documentation/geocoding-api/address/get/
     for Mapquest Geocoding API URL formatting requirements.
     """
-    pass
-
+    place_name = place_name.replace(' ', "%20")
+    url = f"http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={place_name}"
+    response_data = get_json(url)
+    Lat = response_data['results'][0]['locations'][0]['latLng']['lat']
+    Lng = response_data['results'][0]['locations'][0]['latLng']['lng']
+    return Lat, Lng
 
 def get_nearest_station(latitude, longitude):
     """
@@ -37,7 +46,11 @@ def get_nearest_station(latitude, longitude):
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
     formatting requirements for the 'GET /stops' API.
     """
-    pass
+    url = f"https://api-v3.mbta.com/stops?api_key={MBTA_API_KEY}&sort=distance&filter%5Blatitude%5D={latitude}&filter%5Blongitude%5D={longitude}"
+    response_data = get_json(url)
+    station_name = response_data['data'][0]['attributes']['name']
+    wheelchair_accessible = response_data['data'][0]['attributes']['wheelchair_boarding']
+    return station_name,wheelchair_accessible
 
 
 def find_stop_near(place_name):
@@ -46,14 +59,20 @@ def find_stop_near(place_name):
 
     This function might use all the functions above.
     """
-    pass
+    lat, lng = get_lat_long(place_name)
+    station_name, wheelchair_accessible = get_nearest_station(lat,lng)
+    if wheelchair_accessible == 1:
+        wheelchair_accessible = True
+    else:
+        wheelchair_accessible = False
+    return station_name, wheelchair_accessible
 
 
 def main():
     """
     You can test all the functions here
     """
-    pass
+    print(find_stop_near('natick'))
 
 
 if __name__ == '__main__':
